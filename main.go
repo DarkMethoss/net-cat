8d7d08d931419661692c12cb9e74fc5cb51260a6
DarkMethoss <ayoubstudies01@gmail.com>
2024-12-08 12:57:58 +0100

    go routines

--- main.go
+++ main.go
@@ -1,64 +1,36 @@
 package main
 
-import (
-	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
-
-	s "netcat/internal/server"
-)
-
-// ! strconv not allowed
-// ! check port range & port 0 not allowed
-
-
-
+import "fmt"
 
 func main() {
-	port := ""
-	switch len(os.Args[1:]) {
-	case 0:
-		port = "8989"
-	case 1:
-		port = os.Args[1]
-	default:
-		fmt.Println("[USAGE]: ./TCPChat $port")
-	}
+	done := make(chan struct{}) // channel act as a signal for synchronizing goroutines
+	go pipelines(done)
+	<-done // waits for a task to complete
+}
 
-	server, err := s.NewTcpChatServer(port)
-	if err != nil {
-		server.LogError.Println(err)
-	} else {
-		server.LogInfo.Println("")
-	}
+func pipelines(done chan struct{}) {
+	numbers := make(chan int)
+	squares := make(chan int)
 
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGINT)
-
-	defer func() {
-		server.Stopped = true
-		server.LogInfo.Println("Shutting down server...")
-		server.Listener.Close()
-		server.LogInfo.Println("Server stopped gracefully.")
-	}()
-
-	// Creation of a socket
 	go func() {
-		for {
-			if server.Stopped {
-				return
-			}
-			conn, err := server.Listener.Accept()
-			if err != nil {
-				// server.LogError.Println(err)
-				continue
-			}
-			go server.HandleConnection(conn)
+		for i := 0; i < 5; i++ {
+			numbers <- i
 		}
+		close(numbers)
+	}()
+	go func() {
+		for num := range numbers {
+			squares <- num * num
+		}
+		close(squares)
 	}()
 
-	fmt.Println("Main function starts. Waiting for signal...")
-	<-sigChan // Block here until signal is received
-	fmt.Println("Signal received. Exiting.")
+	for square := range squares {
+		fmt.Println(square)
+	}
+	done <- struct{}{} // signal completion
+}
+
+func worker() {
+	
 }
